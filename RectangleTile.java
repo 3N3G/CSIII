6400fc1d0401e4a3a0da1a1c0474af14cc45cf30
3N3G <68242632+3N3G@users.noreply.github.com>
2020-07-22 09:22:42 -0700

    Add files via upload

---
+++ RectangleTile.java
@@ -0,0 +1,55 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class RectangleTile extends Tile{
+	/**
+	 * Constructs a rectangle tile using the super's constructor.
+	 * Because the rectangle is the same as it's border, we can directly call super
+	 * without any changes.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param w
+	 * @param h
+	 * @param c
+	 */
+	public RectangleTile(int x, int y, int w, int h, Color c) {
+		super(x, y, w, h, c);
+	}
+
+	/**
+	 * Draws the given rectangle. For some reason, only draws half the border
+	 */
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.drawRect(getX(), getY(), getWidth(), getHeight());
+      g.setColor(getColor());
+		g.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
+
+	/**
+	 * Gives whether the rectangle contains the given point, by ensuring the x value is
+	 * greater then the x value of the leftmost side, and less than the rightmost side,
+	 * and similarly for y value.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return whether the rectangle contains the point with that xy value.
+	 */
+	@Override
+	public boolean isHit(int x, int y) {
+		boolean inX = (super.getX() < x) && (x < super.getX()+super.getWidth());
+		boolean inY = (super.getY() < y) && (y < super.getY()+super.getHeight());
+		return inX && inY;
+	}
+	
+	/**
+	 * @return the toString of the rectangle, such as: "Rectangle: x=1,y=4,w=2,h=7".
+	 */
+	@Override
+	public String toString() {
+		return "Rectangle: " + super.toString();
+	}
+
+}
